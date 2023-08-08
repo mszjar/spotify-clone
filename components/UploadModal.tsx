98f903c00b471c3a8f1eb4f8b485dca48ba9a847
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-08-08 14:51:31 +0200

    auth with github added

---
+++ components/UploadModal.tsx
@@ -0,0 +1,25 @@
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      uploadModal.onClose();
+    }
+  }
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload an mp3 file."
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      Form
+    </Modal>
+   );
+}
+
+export default UploadModal;
